28ed5fe52273560a4e0766822ddd09a454ca625f
Votre nom <barberhb6@gmail.com>
2021-01-15 10:15:09 +0100

    add step1

---
+++ my workshopC/step 2/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "next.h"
+
+int main()
+{
+    int x , y ;
+    fn(x,y);
+
+    return 0;
+}
---
+++ my workshopC/step 2/next.h
@@ -0,0 +1,13 @@
+void fn(int a ,int b){
+     bool i;
+     printf("enter n1 :");
+     scanf("%d",&a);
+     printf("enter n2 :");
+     scanf("%d",&b);
+     if( i = a>b){
+         printf("la grande value et : a");
+     }else{
+         printf("la grande value et : b ");
+     }
+
+ }
